9fb7e087c29f1b8a65e0690f95fb0efb8aa8b72b
Facts <81759979+Faacts@users.noreply.github.com>
2023-01-30 21:42:40 +0100

    Update Modules.lua

--- Modules.lua
+++ Modules.lua
@@ -117,8 +117,7 @@ function module:antikick(ignoreSetting)
         local NameCallMethod = getnamecallmethod()
 
         if tostring(string.lower(NameCallMethod)) == "kick" then
-            Notify("Pearl","You almost got kicked! Successfully prevented.",5)
-            return nil
+            return self:Notify("Pearl","You almost got kicked! Successfully prevented.",5)
         end
         
         return OldNameCall(Self, ...)
@@ -149,6 +148,196 @@ function module:formatNum(num,formatType)
     end
     if formatType == "Abb" then
         return Abbreviated(num)
+    end
+end
+
+function module:tableF()
+    local TableUtil = {}
+    local function CopyTable(t)
+        assert(type(t) == "table", "First argument must be a table")
+        local tCopy = table.create(#t)
+        for k,v in pairs(t) do
+            if (type(v) == "table") then
+                tCopy[k] = CopyTable(v)
+            else
+                tCopy[k] = v
+            end
+        end
+        return tCopy
+    end
+
+
+    local function Print(tbl, label, deepPrint)
+
+        assert(type(tbl) == "table", "First argument must be a table")
+        assert(label == nil or type(label) == "string", "Second argument must be a string or nil")
+        
+        label = (label or "TABLE")
+        
+        local strTbl = {}
+        local indent = " - "
+        
+        local function Insert(s, l)
+            strTbl[#strTbl + 1] = (indent:rep(l) .. s .. "\n")
+        end
+        
+        local function AlphaKeySort(a, b)
+            return (tostring(a.k) < tostring(b.k))
+        end
+        
+        local function PrintTable(t, lvl, lbl)
+            Insert(lbl .. ":", lvl - 1)
+            local nonTbls = {}
+            local tbls = {}
+            local keySpaces = 0
+            for k,v in pairs(t) do
+                if (type(v) == "table") then
+                    table.insert(tbls, {k = k, v = v})
+                else
+                    table.insert(nonTbls, {k = k, v = "[" .. typeof(v) .. "] " .. tostring(v)})
+                end
+                local spaces = #tostring(k) + 1
+                if (spaces > keySpaces) then
+                    keySpaces = spaces
+                end
+            end
+            table.sort(nonTbls, AlphaKeySort)
+            table.sort(tbls, AlphaKeySort)
+            for _,v in ipairs(nonTbls) do
+                Insert(tostring(v.k) .. ":" .. (" "):rep(keySpaces - #tostring(v.k)) .. v.v, lvl)
+            end
+            if (deepPrint) then
+                for _,v in ipairs(tbls) do
+                    PrintTable(v.v, lvl + 1, tostring(v.k) .. (" "):rep(keySpaces - #tostring(v.k)) .. " [Table]")
+                end
+            else
+                for _,v in ipairs(tbls) do
+                    Insert(tostring(v.k) .. ":" .. (" "):rep(keySpaces - #tostring(v.k)) .. "[Table]", lvl)
+                end
+            end
+        end
+        
+        PrintTable(tbl, 1, label)
+        
+        print(table.concat(strTbl, ""))
+    end
+
+
+    local function Reverse(tbl)
+        local n = #tbl
+        local tblRev = table.create(n)
+        for i = 1,n do
+            tblRev[i] = tbl[n - i + 1]
+        end
+        return tblRev
+    end
+
+
+    local function Shuffle(tbl)
+        assert(type(tbl) == "table", "First argument must be a table")
+        local rng = Random.new()
+        for i = #tbl, 2, -1 do
+            local j = rng:NextInteger(1, i)
+            tbl[i], tbl[j] = tbl[j], tbl[i]
+        end
+    end
+
+
+    local function IsEmpty(tbl)
+        return (next(tbl) == nil)
+    end
+
+
+    TableUtil.Copy = CopyTable
+    TableUtil.Print = Print
+    TableUtil.Reverse = Reverse
+    TableUtil.Shuffle = Shuffle
+
+    return TableUtil
+end
+
+local flingtbl = {}
+local flingA = true
+function module:fling(Value)
+    if flingA then
+        flingA = false
+        return
+    end
+    if not Value then
+        --disable
+        local char = getchar()
+        local rootpart = char.HumanoidRootPart
+        if not rootpart then return end
+        flingtbl.OldPos = rootpart.CFrame
+        local Charr = char:GetChildren()
+        if flingtbl.bv ~= nil then
+            flingtbl.bv:Destroy()
+            flingtbl.bv = nil
+        end
+        if flingtbl.Noclipping2 ~= nil then
+            flingtbl.Noclipping2:Disconnect()
+            flingtbl.Noclipping2 = nil
+        end
+        for i, v in next, Charr do
+            if v:IsA("BasePart") then
+                v.CanCollide = true
+                v.Massless = false
+            end
+        end
+        flingtbl.isRunning = game:GetService("RunService").Stepped:Connect(function()
+            if flingtbl.OldPos then
+                rootpart.CFrame = flingtbl.OldPos
+            end
+            if flingtbl.OldVelocity then
+                rootpart.Velocity = flingtbl.OldVelocity
+            end
+        end)
+        rootpart.RotVelocity = Vector3.new(0,0,0)
+        rootpart.Velocity = Vector3.new(0,0,0)
+        task.wait(2)
+        rootpart.Anchored = true
+        if flingtbl.isRunning then
+            flingtbl.isRunning:Disconnect()
+            flingtbl.isRunning = nil
+        end
+        rootpart.Anchored = false
+        if flingtbl.OldVelocity then
+            rootpart.Velocity = flingtbl.OldVelocity
+        end
+        if flingtbl.OldPos then
+            rootpart.CFrame = flingtbl.OldPos
+        end
+        task.wait()
+        flingtbl.OldVelocity = nil
+        flingtbl.OldPos = nil
+    else
+        --enable
+
+        local char = getchar()
+        local rootpart = char.HumanoidRootPart
+        if not rootpart then return end
+        flingtbl.OldVelocity = rootpart.Velocity
+        local bv = Instance.new("BodyAngularVelocity")
+        flingtbl.bv = bv
+        bv.MaxTorque = Vector3.new(1, 1, 1) * math.huge
+        bv.P = math.huge
+        bv.AngularVelocity = Vector3.new(0, 9e5, 0)
+        bv.Parent = rootpart
+        local Charr = char:GetChildren()
+        for i, v in next, Charr do
+            if v:IsA("BasePart") then
+                v.CanCollide = false
+                v.Massless = true
+                v.Velocity = Vector3.new(0, 0, 0)
+            end
+        end
+        flingtbl.Noclipping2 = game:GetService("RunService").Stepped:Connect(function()
+            for i, v in next, Charr do
+                if v:IsA("BasePart") then
+                    v.CanCollide = false
+                end
+            end
+        end)
     end
 end
 
